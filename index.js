1d7dd0353ee60dcb19f7f34d79562941e1bc5536
Apple <apple@Apples-MacBook-Pro-105.local>
2019-06-10 16:13:11 -0500

    version 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 
-var countDownDate = new Date("June 14 2019 17:38:00").getTime();
+var countDownDate = new Date("June 14 2019 15:38:00").getTime();
 
 var x = setInterval(function() {
 
@@ -13,8 +13,8 @@ var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-document.getElementById("demo").innerHTML = days + "days " + hours + "hours "
- + minutes + "minutes " + seconds + "seconds ";
+document.getElementById("demo").innerHTML = days + " days " + hours + " hours "
+ + minutes + " minutes " + seconds + " seconds ";
 
  if (distance < 0) {
    clearInterval(x);
